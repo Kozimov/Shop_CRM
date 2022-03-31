3aacf50a86106cd171dd55c40f6576881d89f4a1
Kozimov <kozimovziyoxiddin1415@gmail.com>
2022-03-31 15:43:12 +0500

    created user abs

--- leads/admin.py
+++ leads/admin.py
@@ -1,3 +1,6 @@
+import imp
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Lead)
+admin.site.register(User)
--- leads/models.py
+++ leads/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    pass
 
 
 class Lead(models.Model):
