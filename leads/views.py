f0aac49bab72942963f9bd8fdd27a4e6f506fd16
Kozimov <kozimovziyoxiddin1415@gmail.com>
2022-04-02 11:44:17 +0500

    404 and details

--- leads/views.py
+++ leads/views.py
@@ -1,10 +1,16 @@
 from multiprocessing import context
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from . import models
 
-def leads(request):
+def leads_lists(request):
     leads = models.Lead.objects.all()
     context = {
         "leads": leads
     }
     return render(request, "leads_lists.html", context)
+
+
+def leads_detail(request, pk):
+    lead = get_object_or_404(models.Lead, id=pk)
+    return render(request, 'details.html')
