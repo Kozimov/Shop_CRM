769b6f8ad533e99820edec6d500247451d8b284c
Kozimov <kozimovziyoxiddin1415@gmail.com>
2022-04-02 13:13:49 +0500

    token post met

--- leads/views.py
+++ leads/views.py
@@ -1,6 +1,7 @@
 from multiprocessing import context
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
+from .forms import *
 from . import models
 
 def leads_lists(request):
@@ -11,9 +12,16 @@ def leads_lists(request):
     return render(request, "leads_lists.html", context)
 
 
-def leads_detail(request, pk):
+def lead_detail(request, pk):
     lead = get_object_or_404(models.Lead, id=pk)
     context = {
         "lead": lead
     }
     return render(request, 'details.html', context)
+
+def lead_create(request):
+    print(request.POST)
+    context = {
+        "forms": LeadForm
+    }
+    return render(request, "create.html", context)
