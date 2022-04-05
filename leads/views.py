676ad06a0bebb522a06b2e6047c8acfb6241809a
Kozimov <kozimovziyoxiddin1415@gmail.com>
2022-04-05 11:05:49 +0500

    delete lead

--- leads/views.py
+++ leads/views.py
@@ -44,3 +44,8 @@ def lead_update(request, pk):
         "lead": lead,
     }
     return render(request, "update.html", context)
+
+def lead_delete(request, pk):
+    lead = models.Lead.objects.get(id=pk)
+    lead.delete()
+    return redirect("/leads")
